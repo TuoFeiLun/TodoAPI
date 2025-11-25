8b653876626d3e38baf21b3d4f150813e49c5a62
jimmy <ituofeilun@qq.com>
2025-11-25 11:19:06 +1000

    revise route function parameters.

--- TodoEndpoints/TodoEndpoints.cs
+++ TodoEndpoints/TodoEndpoints.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using MyApi.Controller;
+using MyApi.Database;
+using MyApi.DTOs;
 namespace MinAPISeparateFile;
 
 public static class TodoEndpoints
@@ -8,19 +10,28 @@ public static class TodoEndpoints
     {
         var todoItems = app.MapGroup("/todoitems");
 
-        todoItems.MapGet("/", [Authorize("viewer_todoitem")] () => TodoCrud.GetAllTodos);
-        todoItems.MapGet("/complete", [Authorize("viewer_todoitem")] () => TodoCrud.GetCompleteTodos);
-        todoItems.MapGet("/{id}", [Authorize("viewer_todoitem")] () => TodoCrud.GetTodo);
-        todoItems.MapPost("/", [Authorize("crud_todoitem")] () => TodoCrud.CreateTodo);
-        todoItems.MapPut("/{id}", [Authorize("crud_todoitem")] () => TodoCrud.UpdateTodo);
-        todoItems.MapDelete("/{id}", [Authorize("crud_todoitem")] () => TodoCrud.DeleteTodo);
+        todoItems.MapGet("/", [Authorize("viewer_todoitem")] (TodoDb db) =>
+            TodoCrud.GetAllTodos(db));
+        todoItems.MapGet("/complete", [Authorize("viewer_todoitem")] (TodoDb db) =>
+            TodoCrud.GetCompleteTodos(db));
+        todoItems.MapGet("/{id}", [Authorize("viewer_todoitem")] (int id, TodoDb db) =>
+            TodoCrud.GetTodo(id, db));
+        todoItems.MapPost("/", [Authorize("crud_todoitem")] (TodoItemDTO todoItemDTO, TodoDb db) =>
+            TodoCrud.CreateTodo(todoItemDTO, db));
+        todoItems.MapPut("/{id}", [Authorize("crud_todoitem")] (int id, TodoItemDTO todoItemDTO, TodoDb db) =>
+            TodoCrud.UpdateTodo(id, todoItemDTO, db));
+        todoItems.MapDelete("/{id}", [Authorize("crud_todoitem")] (int id, TodoDb db) =>
+            TodoCrud.DeleteTodo(id, db));
 
 
         var adminItems = app.MapGroup("/admin/todoitems");
 
-        adminItems.MapGet("/", TodoCrud.GetAllTodosAdmin);
-        adminItems.MapGet("/{id}", TodoCrud.GetTodoAdmin);
-        adminItems.MapPost("/", TodoCrud.CreateTodoAdmin);
+        adminItems.MapGet("/", (TodoDb db) =>
+            TodoCrud.GetAllTodosAdmin(db));
+        adminItems.MapGet("/{id}", (int id, TodoDb db) =>
+            TodoCrud.GetTodoAdmin(id, db));
+        adminItems.MapPost("/", (TodoAdminDTO todoAdminDTO, TodoDb db) =>
+            TodoCrud.CreateTodoAdmin(todoAdminDTO, db));
     }
 
 }
